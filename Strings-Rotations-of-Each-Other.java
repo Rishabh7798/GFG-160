837a3d42c394a6289f7abbbae6cc10be10b36a29
Rishabh Giri <giririshabh98@gmail.com>
2025-04-11 23:55:49 +0530

    Update Strings-Rotations-of-Each-Other.java

--- Strings-Rotations-of-Each-Other.java
+++ Strings-Rotations-of-Each-Other.java
@@ -1,1 +1,90 @@
 Strings Rotations of Each Other - https://www.geeksforgeeks.org/batch/gfg-160-problems/track/string-gfg-160/problem/check-if-strings-are-rotations-of-each-other-or-not-1587115620
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+
+        // taking input using BufferedReader class
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // taking total count of testcases
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            // Reading the two Strings
+            String s1 = br.readLine();
+            String s2 = br.readLine();
+
+            // Creating an object of class Rotate
+            Solution obj = new Solution();
+
+            // calling areRotations method
+            // of class Rotate and printing
+            //"true" if it returns true
+            // else "false"
+            if (obj.areRotations(s1, s2)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+    }
+}
+
+class Solution {
+    // Function to check if two strings are rotations of each other or not.
+    public static boolean areRotations(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        String combined = s1 + s1;
+        return kmpSearch(combined, s2);
+    }
+
+    // KMP algorithm for substring search
+    private static boolean kmpSearch(String text, String pattern) {
+        int[] lps = computeLPSArray(pattern);
+        int i = 0, j = 0;
+        int n = text.length();
+        int m = pattern.length();
+
+        while (i < n) {
+            if (text.charAt(i) == pattern.charAt(j)) {
+                i++; j++;
+                if (j == m) return true; // match found
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Preprocess pattern to compute longest prefix suffix (LPS) array
+    private static int[] computeLPSArray(String pattern) {
+        int m = pattern.length();
+        int[] lps = new int[m];
+        int len = 0;
+        int i = 1;
+
+        while (i < m) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
