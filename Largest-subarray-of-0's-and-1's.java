00e79726a732dacfd7237d9506486ca757c61309
Rishabh Giri <giririshabh98@gmail.com>
2025-05-24 10:14:45 +0530

    Create Largest-subarray-of-0's-and-1's.java

---
+++ Largest-subarray-of-0's-and-1's.java
@@ -0,0 +1,35 @@
+Largest subarray of 0's and 1's - https://www.geeksforgeeks.org/batch/gfg-160-problems/track/prefix-sum-gfg-160/problem/largest-subarray-of-0s-and-1s
+
+import java.util.HashMap;
+
+class Solution {
+    public int maxLen(int[] arr) {
+        // Replace 0s with -1s
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = -1;
+            }
+        }
+
+        // Map to store (sum -> first index)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (sum == 0) {
+                maxLen = i + 1;
+            }
+
+            if (map.containsKey(sum)) {
+                maxLen = Math.max(maxLen, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+}
